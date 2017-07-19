4c121199fc24b805ad79e402c30d7143915c58c8
Elon Musk <dennis@threadstack.co>
2017-07-19 20:06:29 +0300

    Add app details

--- app/src/main/java/com/patriot/ur254/activities/AboutUsActivity.java
+++ app/src/main/java/com/patriot/ur254/activities/AboutUsActivity.java
@@ -1,12 +1,17 @@
 package com.patriot.ur254.activities;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.patriot.ur254.R;
+
+import java.util.Calendar;
 
 public class AboutUsActivity extends AppCompatActivity {
 
@@ -14,6 +19,8 @@ public class AboutUsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        getVersionInfo();
+        getCopyrightInfo();
         initToolBar();
     }
 
@@ -31,6 +38,30 @@ public class AboutUsActivity extends AppCompatActivity {
             }
         });
         setSupportActionBar(toolbar);
+    }
+
+    private void getVersionInfo() {
+        String versionName = "";
+        int versionCode = -1;
+        try {
+            PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            versionName = packageInfo.versionName;
+            versionCode = packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        TextView textViewVersionInfo = (TextView) findViewById(R.id.textViewVersion);
+        textViewVersionInfo.setText("Version " + versionName);
+    }
+
+    private void getCopyrightInfo(){
+        String copyright = getResources().getString(R.string.copyright);
+        String appName = getResources().getString(R.string.app_name);
+        String currentYear = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+
+        TextView textViewCopyright = (TextView) findViewById(R.id.textViewCopyright);
+        textViewCopyright.setText(copyright  + currentYear + " " + appName + "." + " All rights reserved");
     }
 
 }
