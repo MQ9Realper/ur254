d00c39c8ba6678239b46c9ca166a90d3968961d5
xts400 <dennis@threadstack.co>
2017-07-19 18:33:44 +0300

    Add gallery fragments

--- app/src/main/java/com/patriot/ur254/fragments/GalleryFragment.java
+++ app/src/main/java/com/patriot/ur254/fragments/GalleryFragment.java
@@ -1,30 +1,82 @@
 package com.patriot.ur254.fragments;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.patriot.ur254.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GalleryFragment extends Fragment {
-
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     public GalleryFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gallery, container, false);
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+        tabLayout.setTabTextColors(Color.parseColor("#4c4c4c"), Color.parseColor("#000000"));
+        tabLayout.setupWithViewPager(viewPager);
+        SetUpViewpager(viewPager);
+
+        return view;
+    }
+
+    private void SetUpViewpager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getChildFragmentManager());
+        adapter.addFragment(new PhotosFragment(), "PHOTOS");
+        adapter.addFragment(new VideosFragment(), "VIDEOS");
+        viewPager.setAdapter(adapter);
+    }
+
+    private class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        private ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        private void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
+
     }
 
 }
