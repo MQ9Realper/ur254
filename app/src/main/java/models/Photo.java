3bbd24484fd4a5d9d7b737b5af88679afb713c17
xts400 <dennis@threadstack.co>
2017-07-20 17:59:32 +0300

    Add data model for photos

--- app/src/main/java/models/Photo.java
+++ app/src/main/java/models/Photo.java
@@ -4,5 +4,28 @@ package models;
  * Created by dennismwebia on 7/20/17.
  */
 
-public class Photos {
+public class Photo {
+    private String photo_title;
+    private Integer photo;
+
+    public Photo(String photo_title, Integer photo){
+        this.photo_title = photo_title;
+        this.photo = photo;
+    }
+
+    public String getPhoto_title() {
+        return photo_title;
+    }
+
+    public void setPhoto_title(String photo_title) {
+        this.photo_title = photo_title;
+    }
+
+    public Integer getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Integer photo) {
+        this.photo = photo;
+    }
 }
