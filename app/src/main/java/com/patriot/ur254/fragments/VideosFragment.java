e714916d03144445b03a73bf940e06b09df5d059
xts400 <dennis@threadstack.co>
2017-07-20 15:34:51 +0300

    Add youtube components

--- app/src/main/java/com/patriot/ur254/fragments/VideosFragment.java
+++ app/src/main/java/com/patriot/ur254/fragments/VideosFragment.java
@@ -3,16 +3,22 @@ package com.patriot.ur254.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.patriot.ur254.R;
+import com.patriot.ur254.adapters.VideosAdapter;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class VideosFragment extends Fragment {
+
+    private View viewVideos;
 
 
     public VideosFragment() {
@@ -24,7 +30,18 @@ public class VideosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_videos, container, false);
+        viewVideos = inflater.inflate(R.layout.fragment_videos, container, false);
+        //setVideoList();
+        setUpRecyclerView();
+        return viewVideos;
+    }
+
+    private void setUpRecyclerView() {
+        RecyclerView recyclerView = (RecyclerView) viewVideos.findViewById(R.id.listView);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(new VideosAdapter(getActivity()));
     }
 
 }
