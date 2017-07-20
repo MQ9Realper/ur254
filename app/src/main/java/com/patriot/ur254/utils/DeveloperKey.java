bc98f2bf35974fa53f7c1ff1de78cd0f772053e9
xts400 <dennis@threadstack.co>
2017-07-20 14:47:48 +0300

    Add developer key class

---
+++ app/src/main/java/com/patriot/ur254/utils/DeveloperKey.java
@@ -0,0 +1,10 @@
+package com.patriot.ur254.utils;
+
+/**
+ * Created by dennismwebia on 7/20/17.
+ */
+
+public class DeveloperKey {
+    public static final String DEVELOPER_KEY = "AIzaSyCfsrmqtgwDOj2OABFxkPpYyMQR__u6tGc";
+}
+
