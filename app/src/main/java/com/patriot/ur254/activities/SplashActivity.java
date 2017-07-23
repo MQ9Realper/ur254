a7ba8b167d309024975b1173408186fe7dd4799a
Elon Musk <dennis@threadstack.co>
2017-07-23 16:00:19 +0300

    Add background image

---
+++ app/src/main/java/com/patriot/ur254/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.patriot.ur254.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.patriot.ur254.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
