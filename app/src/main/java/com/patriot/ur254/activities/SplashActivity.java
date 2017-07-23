169000a36564a387af6bd35f5cba7fdaee974e5f
Elon Musk <dennis@threadstack.co>
2017-07-23 16:33:35 +0300

    Add splash screen activity

--- app/src/main/java/com/patriot/ur254/activities/SplashActivity.java
+++ app/src/main/java/com/patriot/ur254/activities/SplashActivity.java
@@ -1,9 +1,13 @@
 package com.patriot.ur254.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.patriot.ur254.R;
+import com.patriot.ur254.utils.SharedPreference;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,5 +15,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        SharedPreference sharedPreference = new SharedPreference(this);
+
+        if (sharedPreference.getIsLoggedIn()) {
+            Intent intentTimeline = new Intent(SplashActivity.this, TimelineActivity.class);
+            startActivity(intentTimeline);
+            finish();
+        }
+
+        RelativeLayout layoutGetStarted = (RelativeLayout) findViewById(R.id.layoutGetStarted);
+        layoutGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentStart = new Intent(SplashActivity.this, StartActivity.class);
+                startActivity(intentStart);
+            }
+        });
     }
 }
